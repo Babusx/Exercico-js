da874cff7f0fe134584df3b1814c6b9f1d9acffd
guilherme de oliveira macedo <go479724@gmail.com>
2023-01-30 10:38:32 -0300

    java

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = "Zé Comeia " // STRING
+let idade = "20" // INT OU NUMBER
+let altura = 1.85 // FLOAT OU NUMBER
+let temGato = true // BOOLEAN
+const pi = 3.1415
+
+console.log(" Oi eu sou " +nome+ " e eu tenho " +idade+" anos ")
+console.log(` Oi eu sou ${nome} e eu tenho ${idade} anos `)
